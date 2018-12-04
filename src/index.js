c14222cdef425ef9be378af19d84032deea3baa9
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2018-12-04 11:01:44 +0200

    fixed index.js imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import FileUploadForm from './FileUploadForm';
 
 ReactDOM.render(<FileUploadForm/>, document.getElementById('app-root'));
